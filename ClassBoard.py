0c0f0fa41322a1477763b0018090adf73a703640
valentinbarzola <v.barzola@alumno.um.edu.ar>
2024-08-14 20:50:16 -0300

    Agregado de todas las fichas en sus posiciones

--- ClassBoard.py
+++ ClassBoard.py
@@ -1,4 +1,4 @@
-from ClassChess import Rook,pieces #importar la clase Rook y pieces de ClassChess.py
+from ClassChess import Rook,Horse,Bishop,Queen,King,Pawn,Piece #importar la clase Rook y pieces de ClassChess.py
 
 class board():
     def __init__(self, nombre, color):
@@ -14,6 +14,22 @@ class board():
         self.__positions__[7][7]= Rook("White") #white
         self.__positions__[0][7]= Rook("Black") #black
         self.__positions__[7][0]= Rook("White") #White
+        self.__positions__[0][1]= Horse("Black")
+        self.__positions__[0][6]= Horse("Black")
+        self.__positions__[7][1]= Horse("White")
+        self.__positions__[7][6]= Horse("White")
+        self.__positions__[0][2]= Bishop("Black")
+        self.__positions__[0][5]= Bishop("Black")
+        self.__positions__[7][2]= Bishop("White")
+        self.__positions__[7][5]= Bishop("White")
+        self.__positions__[0][3]= Queen("Black")
+        self.__positions__[7][3]= Queen("White")
+        self.__positions__[0][4]= King("Black")
+        self.__positions__[7][4]= King("White")
+        #Llenado de pawns
+        for i in range(8):
+            self.__positions__[1][i]= Pawn("Black")
+            self.__positions__[6][i]= Pawn("White")
         self.__nombre__ = nombre
         self.__color__ = color
 
