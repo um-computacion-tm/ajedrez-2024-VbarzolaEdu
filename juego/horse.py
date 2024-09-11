1858a5af5597c10f2d826cdcdce40a38b3558787
valentinbarzola <v.barzola@alumno.um.edu.ar>
2024-09-11 00:11:15 -0300

    addign horse movements

--- juego/horse.py
+++ juego/horse.py
@@ -11,3 +11,24 @@ class Horse(Piece):
         if abs(from_row - to_row)==1 and abs(from_col - to_col)==2:
             return True
         return False
+    
+    def valid_positions_horse(self,from_row,from_col,to_row,to_col):
+        possible_positions=(self.possible_positions_down(from_row,from_col)+self.possible_positions_up(from_row,from_col)+self.possible_positions_down(from_row,from_col))
+        return (to_row,to_col) in possible_positions
+    
+    def possible_positions(self,row,col,kr,kc):
+        possibles=[]
+        if 0 <= row+kr < 8 and 0 <= col+kc < 8:
+            other_piece=self.__board__.get_piece(row+kr,row+kc)
+            if other_piece is not None:
+                if other_piece.__color__!=self.__color__:
+                    possibles.append((row+kr,col+kc))
+            if self.__board__.get_piece(row+kr,row+kc) is None:  
+                possibles.append((row+kr,col+kc))
+        return possibles
+    
+    def possible_positions_down(self,row,col):
+        return self.possible_positions(row,col,2,3)+self.possible_positions(row,col,2,-3)+self.possible_positions(row,col,3,2)+self.possible_positions(row,col,3,-2)
+    
+    def possible_positions_up(self,row,col):
+        return self.possible_positions(row,col,-2,3)+self.possible_positions(row,col,-2,-3)+self.possible_positions(row,col,-3,2)+self.possible_positions(row,col,-3,-2)
