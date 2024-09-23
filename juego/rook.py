cef6ae927e509447abe8ca643a2c5106040efc30
valentinbarzola <v.barzola@alumno.um.edu.ar>
2024-09-23 11:40:45 -0300

    fixing codeclimate

--- juego/rook.py
+++ juego/rook.py
@@ -8,7 +8,12 @@ class Rook(Piece):
     White_str = "♜"
     Black_str = "♖"
     
+    # def valid_positions_rook(self,from_row,from_col,to_row,to_col):
+    #    return self.valid_positions_straight(from_row,from_col,to_row,to_col) 
+
     def valid_positions_rook(self,from_row,from_col,to_row,to_col):
-       return self.valid_positions_straight(from_row,from_col,to_row,to_col) 
+        possible_positions=(self.possible_positions_horizontal(from_row,from_col,1,8,1)+self.possible_positions_vertical(from_row,from_col,1,1,8)+self.possible_positions_horizontal(from_row,from_col,-1,-1,-1)+self.possible_positions_vertical(from_row,from_col,-1,-1,-1))
+        
+        return (to_row, to_col) in possible_positions
     
     
