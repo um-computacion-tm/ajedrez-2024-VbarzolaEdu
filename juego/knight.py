d5bc1830e1926406af8d1ee708c214400578c340
valentinbarzola <v.barzola@alumno.um.edu.ar>
2024-09-24 19:49:10 -0300

    codeclimate

--- juego/knight.py
+++ juego/knight.py
@@ -6,6 +6,22 @@ class Knight(Piece):
     Black_str = "♘"
         
 
+    # def valid_positions_knight(self, from_row, from_col, to_row, to_col):
+    #     """
+    #     Verifica si un movimiento es válido para el Knight.
+
+    #     Args:
+    #         from_row (int): La fila de origen del Knight.
+    #         from_col (int): La columna de origen del Knight.
+    #         to_row (int): La fila de destino del Knight.
+    #         to_col (int): La columna de destino del Knight.
+
+    #     Returns:
+    #         bool: True si el movimiento es válido, False en caso contrario.
+    #     """
+    #     possible_positions = (self.possible_positions_one_move(from_row, from_col,2,1)+ self.possible_positions_one_move(from_row, from_col,2,-1) + self.possible_positions_one_move(from_row, from_col,1,2) + self.possible_positions_one_move(from_row, from_col,1,-2) + self.possible_positions_one_move(from_row, from_col,-2,1) + self.possible_positions_one_move(from_row, from_col,-2,-1) + self.possible_positions_one_move(from_row, from_col,-1,2) + self.possible_positions_one_move(from_row, from_col,-1,-2))
+    #     return (to_row, to_col) in possible_positions
+
     def valid_positions_knight(self, from_row, from_col, to_row, to_col):
         """
         Verifica si un movimiento es válido para el Knight.
@@ -19,7 +35,17 @@ class Knight(Piece):
         Returns:
             bool: True si el movimiento es válido, False en caso contrario.
         """
-        possible_positions = (self.possible_positions_one_move(from_row, from_col,2,1)+ self.possible_positions_one_move(from_row, from_col,2,-1) + self.possible_positions_one_move(from_row, from_col,1,2) + self.possible_positions_one_move(from_row, from_col,1,-2) + self.possible_positions_one_move(from_row, from_col,-2,1) + self.possible_positions_one_move(from_row, from_col,-2,-1) + self.possible_positions_one_move(from_row, from_col,-1,2) + self.possible_positions_one_move(from_row, from_col,-1,-2))
-        return (to_row, to_col) in possible_positions
+        knight_moves = [
+            (2, 1), (2, -1), (1, 2), (1, -2),
+            (-2, 1), (-2, -1), (-1, 2), (-1, -2)
+        ]
 
-  
+        possible_positions = []
+        for d_row, d_col in knight_moves:
+            possible_positions += self.possible_positions_one_move(from_row, from_col, d_row, d_col)
+    
+        # Devolver si la posición de destino está en las posibles posiciones
+        return (to_row, to_col) in possible_positions
+    
+
+    
